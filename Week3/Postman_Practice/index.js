189333fa2d3cb1fb416a1263265f3106e532287a
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-10-16 23:39:50 -0400

    first commit

--- Week3/Postman_Practice/index.js
+++ Week3/Postman_Practice/index.js
@@ -1,10 +1,48 @@
-const xhr = new XMLHttpRequest()
+axios.put("https://api.vschool.io/jochy/todo/5f665099a269067180b0f0b8",{title:"new title"})
+.then(res => {
 
-xhr.open("GET", "https://swapi.dev/api/planets/", true)
-xhr.send()
-
-xhr.open("GET", "https://swapi.dev/api/people/", true)
-xhr.send()
-
+    console.log(res.data.description)
+})
+.catch(error=>console.log(error))
 
 
+
+
+axios.post("https://api.vschool.io/jochy/todo",{
+
+    "completed": false,
+        "title": "Movies",
+        "description": "Actions",
+        "price": 0,
+        "imgUrl": "http://path-to-url.jpg",
+}) 
+
+
+axios.get("https://api.vschool.io/jochy/todo")
+.then(res => {
+    for(var i = 0; i<res.data.length; i++){
+        console.log(res.data[i])
+        var div = document.getElementById("api")
+        
+        var listObject = document.createElement("div")
+        div.appendChild(listObject)
+
+        var heading = document.createElement("h1")
+        heading.textContent = res.data[i].title
+        listObject.appendChild(heading)
+
+        var heading = document.createElement("h1")
+        heading.textContent = res.data[i].title
+        listObject.appendChild(heading)
+
+
+
+    }
+
+    console.log(res.data[0].description)
+})
+/* .catch(error=>console.log(error))
+
+axios.get("https://api.vschool.io/jochy/todo/5f665099a269067180b0f0b8")
+
+axios.delete("https://api.vschool.io/jochy/todo/5f665099a269067180b0f0b8") */ 
